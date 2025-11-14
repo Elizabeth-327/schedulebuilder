d42b06497afe3cb714eb7756a0b536364311b54f
addiebart <addiebart@ku.edu>
2025-11-14 11:47:46 -0600

    added updateScheduleList and removeSchedule

--- src/app/supabaseAccess.ts
+++ src/app/supabaseAccess.ts
@@ -18,6 +18,12 @@ export async function addSchedule(
   try {
     await databaseClient.auth.setSession(tokens);
     const current = await getSchedules(tokens, user_uuid);
+    const exists = current.some(
+      (s) => s.name === schedule.name
+    );
+    if (exists) {
+      return false
+    }
     current.push(schedule);
     const { error } = await databaseClient
       .from("Users")
@@ -28,14 +34,13 @@ export async function addSchedule(
       console.error("Could not add schedule:", error.message);
       return false;
     }
-
-    // else
     return true;
   } catch (e) {
-    console.error("Execption in addSchedule", e);
+    console.error("Exception in addSchedule", e);
     return false;
   }
 }
+
 export async function updateSchedule(
   tokens: SupabaseTokens,
   user_uuid: string,
@@ -65,6 +70,61 @@ export async function updateSchedule(
     return true;
   } catch (e) {
     console.error("Exception in updateSchedule", e);
+    return false;
+  }
+}
+
+export async function updateScheduleList(
+  tokens: SupabaseTokens,
+  user_uuid: string,
+  schedules: Schedule[]
+) {
+  try {
+    await databaseClient.auth.setSession(tokens);
+    const { error } = await databaseClient
+      .from("Users")
+      .update({ schedules: schedules as any })
+      .eq("user_uuid", user_uuid);
+
+    if (error) {
+      console.error("Could not update schedules Object:", error.message);
+      return false;
+    }
+    return true;
+  } catch (e) {
+    console.error("Exception in updateScheduleList", e);
+    return false;
+  }
+}
+
+// true on success
+export async function removeSchedule(
+  tokens: SupabaseTokens,
+  user_uuid: string,
+  scheduleName: string
+) {
+  try {
+    await databaseClient.auth.setSession(tokens);
+    const current = await getSchedules(tokens, user_uuid);
+    const removalIdx = current.findIndex((s) => s.name === scheduleName);
+    if (removalIdx === -1) {
+      return false;
+    }
+    current.splice(removalIdx, 1);
+    const { error } = await databaseClient
+      .from("Users")
+      .update({ schedules: current as any })
+      .eq("user_uuid", user_uuid);
+
+    if (error) {
+      console.error("Could not remove schedule:", error.message);
+      return false;
+    }
+
+    // else
+    return true;
+  } catch (e) {
+    console.error("Execption in removeSchedule", e);
     return false;
   }
 }
